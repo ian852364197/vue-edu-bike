055c0ad10a544c35d0400301851b02ff199cacf8
IanTang <iantang@uitc.com.tw>
2024-07-17 11:34:07 +0800

    feat. 引入bootstrap scss

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '@/scss/styles.scss';
 
 import App from './App.vue';
 import router from './router';
