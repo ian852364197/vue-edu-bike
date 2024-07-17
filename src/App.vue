2638cac7d6e720c8dfbea64ff4a7ab95304501c4
IanTang <iantang@uitc.com.tw>
2024-07-17 11:20:50 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <button class="btn" @click="destination = ''">
+              <RouterLink to="/">Home</RouterLink>
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1 class="text-primary">Home</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
