be4421d1f1758a5d39908c0011dcf90e2b97225a
IanTang <iantang@uitc.com.tw>
2024-07-18 10:46:13 +0800

    feat. 製作分頁

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
+import Paginate from 'vuejs-paginate-next';
+
 const bikeList = ref([]);
+
+const page = ref();
+
+const pageRangeList = ref([10, 20]);
+const pageRange = ref(pageRangeList.value[0]);
+const pageCount = computed(() => {
+  return parseInt(bikeList.value.length / pageRange.value) + 1;
+});
+
 onMounted(async () => {
   const test = await fetch(
     'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
@@ -29,7 +40,7 @@ onMounted(async () => {
       </tr>
     </thead>
     <tbody>
-      <tr v-for="station in bikeList" :key="station.sno">
+      <!-- <tr v-for="station in bikeList" :key="station.sno">
         <th scope="row">{{ station.sno }}</th>
         <td>{{ station.sna }}</td>
         <td>{{ station.sarea }}</td>
@@ -39,9 +50,25 @@ onMounted(async () => {
         <td>{{ station.latitude }}</td>
         <td>{{ station.longitude }}</td>
         <td>{{ station.available_return_bikes }}</td>
-      </tr>
+      </tr> -->
     </tbody>
   </table>
+  <Paginate
+    v-model="page"
+    :page-count="pageCount"
+    :page-range="3"
+    :margin-pages="5"
+    :prev-text="'<<'"
+    :next-text="'>>'"
+    :container-class="'pagination'"
+    :page-class="'page-item'"
+    :first-last-button="true"
+  >
+  </Paginate>
+  <label for="range-select">顯示</label>
+  <select v-model="pageRange" id="range-select">
+    <option v-for="range in pageRangeList" :key="range" :value="range">{{ range }}筆</option>
+  </select>
 </template>
 
 <style scoped></style>
