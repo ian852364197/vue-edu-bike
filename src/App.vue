19f39eb9304ff9bd625ab68a7c861a95d9382883
IanTang <iantang@uitc.com.tw>
2024-07-18 17:47:44 +0800

    feat. 些微調整&註解

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,36 @@
 import { computed, onMounted, ref, watch } from 'vue';
 import Paginate from 'vuejs-paginate-next';
 
+//API取得的資料
 const bikeList = ref([]);
 
+//分頁用
 const page = ref(1);
-
 const pageRangeList = ref([10, 20]);
 const pageRange = ref(pageRangeList.value[0]);
 const pageCount = computed(() => {
   return parseInt(filterList.value.length / pageRange.value) + 1;
 });
 
+//排序用
 const rentSort = ref(null);
 const totalSort = ref(null);
-
 const rentIcon = computed(() => compIcon(rentSort));
+const totalIcon = computed(() => compIcon(totalSort));
 
-const returnIcon = computed(() => compIcon(totalSort));
+//搜尋用
+const address = ref('');
 
+//經過搜尋or排序的資料
+const filterList = ref([]);
+//畫面顯示的資料
+const showList = computed(() => {
+  let first = (page.value - 1) * pageRange.value;
+  let last = page.value * pageRange.value;
+  return filterList.value.slice(first, last);
+});
+
+//變換排序圖示
 function compIcon(sortBy) {
   if (!sortBy.value) {
     return '△';
@@ -26,16 +39,7 @@ function compIcon(sortBy) {
   return sortBy.value === 'desc' ? '▲' : '▼';
 }
 
-const address = ref('');
-
-const filterList = ref([]);
-
-const showList = computed(() => {
-  let first = (page.value - 1) * pageRange.value;
-  let last = page.value * pageRange.value;
-  return filterList.value.slice(first, last);
-});
-
+//排序方法
 function sortByRent() {
   totalSort.value = null;
   if (rentSort.value === 'desc') {
@@ -46,11 +50,10 @@ function sortByRent() {
   } else {
     rentSort.value = 'desc';
     filterList.value = filterList.value.sort((a, b) => {
-      return a.available_rent_bikes * -1 - b.available_rent_bikes * -1;
+      return b.available_rent_bikes - a.available_rent_bikes;
     });
   }
 }
-
 function sortByTotal() {
   rentSort.value = null;
   if (totalSort.value === 'desc') {
@@ -66,6 +69,7 @@ function sortByTotal() {
   }
 }
 
+//call api取得資料
 onMounted(async () => {
   const test = await fetch(
     'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
@@ -76,6 +80,7 @@ onMounted(async () => {
   });
 });
 
+//選擇一頁顯示幾筆
 watch(
   () => pageRange.value,
   () => {
@@ -87,6 +92,7 @@ watch(
   { immediate: true }
 );
 
+//模糊搜尋
 watch(
   () => address.value,
   () => {
@@ -124,7 +130,7 @@ watch(
           <th scope="col" class="td">站點名稱</th>
           <th scope="col" class="td">站點所在區域</th>
           <th scope="col" class="td">站點地址</th>
-          <th scope="col" class="td" @click="sortByTotal">總車位數量 {{ returnIcon }}</th>
+          <th scope="col" class="td" @click="sortByTotal">總車位數量 {{ totalIcon }}</th>
           <th scope="col" class="td" @click="sortByRent">可租借的腳踏車數量 {{ rentIcon }}</th>
           <th scope="col" class="td">站點緯度</th>
           <th scope="col" class="td">站點經度</th>
