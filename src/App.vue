1ad38b9363f104c63f7de01b6dc56f96e801bc32
IanTang <iantang@uitc.com.tw>
2024-07-18 12:11:22 +0800

    feat. 站點地址模糊搜尋

--- src/App.vue
+++ src/App.vue
@@ -37,6 +37,8 @@ const returnIcon = computed(() => {
   }
 });
 
+const address = ref('');
+
 const filterList = ref([]);
 
 const showList = computed(() => {
@@ -88,59 +90,94 @@ onMounted(async () => {
 watch(
   () => pageRange.value,
   () => {
-    let now = pageRange.value === 10 ? page.value * 2 : parseInt(page.value / 2);
-    page.value = now;
+    if (page.value !== 1) {
+      let now = pageRange.value === 10 ? page.value * 2 : parseInt(page.value / 2);
+      page.value = now;
+    }
+  },
+  { immediate: true }
+);
+
+watch(
+  () => address.value,
+  () => {
+    let addText = address.value.trim();
+    filterList.value = bikeList.value.filter((station) => station.ar.includes(addText));
   },
   { immediate: true }
 );
 </script>
 
 <template>
-  <h1 class="text-primary">testing</h1>
-  <table class="table table-striped">
-    <thead>
-      <tr>
-        <th scope="col">站點編號</th>
-        <th scope="col">站點名稱</th>
-        <th scope="col">站點所在區域</th>
-        <th scope="col">站點地址</th>
-        <th scope="col">總車位數量</th>
-        <th scope="col" @click="sortByRent">可租借的腳踏車數量 {{ rentIcon }}</th>
-        <th scope="col">站點緯度</th>
-        <th scope="col">站點經度</th>
-        <th scope="col" @click="sortByReturn">可歸還的腳踏車數量 {{ returnIcon }}</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="station in showList" :key="station.sno">
-        <th scope="row">{{ station.sno }}</th>
-        <td>{{ station.sna }}</td>
-        <td>{{ station.sarea }}</td>
-        <td>{{ station.ar }}</td>
-        <td>{{ station.total }}</td>
-        <td>{{ station.available_rent_bikes }}</td>
-        <td>{{ station.latitude }}</td>
-        <td>{{ station.longitude }}</td>
-        <td>{{ station.available_return_bikes }}</td>
-      </tr>
-    </tbody>
-  </table>
-  <Paginate
-    v-model="page"
-    :page-count="pageCount"
-    :page-range="3"
-    :margin-pages="5"
-    :prev-text="'<<'"
-    :next-text="'>>'"
-    :container-class="'pagination'"
-    :page-class="'page-item'"
-    :first-last-button="true"
-  >
-  </Paginate>
-  <label for="range-select">顯示</label>
-  <select v-model="pageRange" id="range-select">
-    <option v-for="range in pageRangeList" :key="range" :value="range">{{ range }}筆</option>
-  </select>
+  <div class="container">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <p class="navbar-brand">Bike 練習</p>
+        <div class="d-flex">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="輸入站點地址"
+            aria-label="Search"
+            v-model="address"
+          />
+          <button class="btn btn-outline-success" type="button">Search</button>
+        </div>
+      </div>
+    </nav>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col" class="td">站點編號</th>
+          <th scope="col" class="td">站點名稱</th>
+          <th scope="col" class="td">站點所在區域</th>
+          <th scope="col" class="td">站點地址</th>
+          <th scope="col" class="td">總車位數量</th>
+          <th scope="col" class="td" @click="sortByRent">可租借的腳踏車數量 {{ rentIcon }}</th>
+          <th scope="col" class="td">站點緯度</th>
+          <th scope="col" class="td">站點經度</th>
+          <th scope="col" class="td" @click="sortByReturn">可歸還的腳踏車數量 {{ returnIcon }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="station in showList" :key="station.sno">
+          <th scope="row">{{ station.sno }}</th>
+          <td>{{ station.sna }}</td>
+          <td>{{ station.sarea }}</td>
+          <td>{{ station.ar }}</td>
+          <td>{{ station.total }}</td>
+          <td>{{ station.available_rent_bikes }}</td>
+          <td>{{ station.latitude }}</td>
+          <td>{{ station.longitude }}</td>
+          <td>{{ station.available_return_bikes }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="d-flex justify-content-evenly">
+      <Paginate
+        v-model="page"
+        :page-count="pageCount"
+        :page-range="3"
+        :margin-pages="5"
+        :prev-text="'<<'"
+        :next-text="'>>'"
+        :container-class="'pagination'"
+        :page-class="'page-item'"
+        :first-last-button="true"
+      >
+      </Paginate>
+      <div>
+        <label for="range-select">顯示</label>
+        <select v-model="pageRange" id="range-select">
+          <option v-for="range in pageRangeList" :key="range" :value="range">{{ range }}筆</option>
+        </select>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.td {
+  width: 8%;
+}
+</style>
